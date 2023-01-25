6a25b0dbcef74bbd4dfe4f171db67786c12ce521
camidaraio <camiladaraio15@gmail.com>
2023-01-25 09:53:56 -0300

    Eventos / onClick / funciones

---
+++ src/Components/EjemploEvento.js
@@ -0,0 +1,12 @@
+export default function EjemploEvento() {
+// Declaro la funcion dentro del componente
+// function saludar () {
+//    alert("Hola");
+// }
+
+    return (
+        <button onClick={() => alert("Holis")}>
+            Cliqueame
+        </button>
+    )
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import CardWrapper from "../Components/CardWrapper"
+import EjemploEvento from "../Components/EjemploEvento"
 
 export default function Home () {
     return (
         <>
         <h1>Estas en la home</h1>
+        <EjemploEvento />
         <CardWrapper 
            titulo={"Ofertas"}
         />
